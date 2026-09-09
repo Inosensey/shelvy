5c3eb6e0374554729cc3c59172faeb890f6c6a21
Inosensey <dingcong.bae@gmail.com>
2026-09-09 11:32:53 +0800

    chore(app): import and register all new modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './PrismaConfig/prisma.module';
+import { StripeModule } from './modules/stripeModule/stripe.module';
+import { SubscriptionModule } from './modules/subscriptionModule/subscription.module';
+import { AuthModule } from './modules/authModule/auth.module';
+import { UserModule } from './modules/usersModule/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ConfigModule,
+    AuthModule,
+    UserModule,
+    StripeModule,
+    SubscriptionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
